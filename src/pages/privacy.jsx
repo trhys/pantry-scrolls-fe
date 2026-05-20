7c69bc2b7929bb9f342f829fd7a795022091e85f
Tim Reese <treese1993@gmail.com>
2026-05-20 11:22:25 -0500

    added privacy policy embedded from docs

--- src/pages/privacy.jsx
+++ src/pages/privacy.jsx
@@ -2,6 +2,6 @@
 export default function PrivacyPolicy() {
 
   return (
-    <p>Not implemented</p>
+    <iframe src="https://docs.google.com/document/d/e/2PACX-1vRrsN69h6qEMnxWnwoDcqOU7jIRvWksCzT79LBG3YwtmZsUBH3FrGHEXmvdRzYkalso5OeKv9iHvhDV/pub?embedded=true"></iframe>
   );
 }
